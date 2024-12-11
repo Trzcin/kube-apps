fd26568f5d9a38ae695066acde226d3f256d3ea0
Mikołaj Trzciński <trzccin@gmail.com>
2024-12-11 12:50:03 +0100

    init backend

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+
+const todos = ["todo 1", "todo 2"];
+app.get("/", (_, res) => {
+   return res.json(todos); 
+});
+
+const port = process.env.PORT ?? 80;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
