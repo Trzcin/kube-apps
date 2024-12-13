5550a699eebab5e50e821b097fb0eac0d44077d8
Mikołaj Trzciński <trzccin@gmail.com>
2024-12-13 23:51:33 +0100

    backend ts update

---
+++ backend/src/server.ts
@@ -0,0 +1,21 @@
+import express, { Router } from 'express'
+
+const app = express();
+const apiRouter = Router();
+app.use((_, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const todos: Todo[] = [
+   { id: crypto.randomUUID(), text: "todo item 1" },
+   { id: crypto.randomUUID(), text: "todo item 2" }
+];
+apiRouter.get("/todos", (_, res) => {
+   res.json(todos); 
+});
+
+app.use("/api", apiRouter);
+const port = process.env.PORT ?? 80;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
