269562b909a0f4addd3603707afe425205aadd56
Mikołaj Trzciński <trzccin@gmail.com>
2024-12-14 16:44:43 +0100

    add websocket endpoint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import express, { Router } from 'express'
+import expressWs from 'express-ws';
 
 const app = express();
+expressWs(app);
 const apiRouter = Router();
 app.use((_, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -14,6 +16,15 @@ const todos: Todo[] = [
 ];
 apiRouter.get("/todos", (_, res) => {
    res.json(todos); 
+});
+
+apiRouter.ws("/ws", (ws) => {
+   ws.on('message', (data) => {
+      const msg = data.toString();
+      if (msg === 'ping') {
+         ws.send('pong');
+      }
+   });
 });
 
 app.use("/api", apiRouter);
